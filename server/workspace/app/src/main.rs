984d043a1c0467bd11f1fa754d7d7e9e199a9ef8
Steven Jimenez <stevenmjimenez@gmail.com>
2023-02-18 03:20:03 -0500

    Bootstrapped server and client.

---
+++ server/workspace/app/src/main.rs
@@ -0,0 +1,23 @@
+#![deny(
+    clippy::complexity,
+    clippy::correctness,
+    clippy::perf,
+    clippy::style,
+    clippy::suspicious,
+    clippy::pedantic
+)]
+
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(hello))
+        .bind(("0.0.0.0", 80))?
+        .run()
+        .await
+}
